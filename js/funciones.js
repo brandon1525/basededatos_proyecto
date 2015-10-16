92f8423ee56b03d0e463e6ddc0ba874cc3818caa
Brandon Rodríguez <br4nd0n4l4n@gmail.com>
2015-10-15 22:24:16 -0500

    ejemplo de como se va a manejar la vista web

--- js/funciones.js
+++ js/funciones.js
@@ -1,6 +1,21 @@
 $( document ).ready(function() {
     $('#btn_buscar').click( function (){
-    	console.log("buscar");
-    	//$('#ocultar').hide();
+    	$.ajax({
+				url: "http://192.168.15.251:3000/negocio",
+			    method: 'GET',
+			    //data: {name: name_business, user_id: user_id},
+			    dataType:'jsonp',
+			    mimeType: 'application/json'
+			}).done(function(jsonp) {
+				console.log(jsonp);
+			  	var result = jsonp.result;
+			  	if(result=="true"){
+			   		//jsonp.data.num_venues = num_venues;
+			   		//saveStorage(JSON.stringify(jsonp.data),"business","session");
+			   		//window.location="datos_personales.html";
+			  	}else{
+			   		alert("Algo salió mal");
+				}
+			});
     });
 });
